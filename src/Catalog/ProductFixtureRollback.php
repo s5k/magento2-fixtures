67b933da0928bb27ca3840a7c643870167bc300e
s5k <40916972+s5k@users.noreply.github.com>
2025-08-08 23:34:43 +0700

    Refactor ProductFixtureRollback: add Filesystem dependency and enhance execute method to delete media gallery images

--- src/Catalog/ProductFixtureRollback.php
+++ src/Catalog/ProductFixtureRollback.php
@@ -1,14 +1,16 @@
 <?php
+
 declare(strict_types=1);
 
 namespace TddWizard\Fixtures\Catalog;
 
 use Magento\Catalog\Api\ProductRepositoryInterface;
-use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Framework\Exception\StateException;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Exception\FileSystemException;
 use Magento\Framework\Registry;
+use Magento\Framework\Filesystem;
 use Magento\TestFramework\Helper\Bootstrap;
+use RuntimeException;
 
 /**
  * @internal Use ProductFixture::rollback() or ProductFixturePool::rollback() instead
@@ -18,39 +20,78 @@ class ProductFixtureRollback
     /**
      * @var Registry
      */
-    private $registry;
+    private Registry $registry;
 
     /**
      * @var ProductRepositoryInterface
      */
-    private $productRepository;
+    private ProductRepositoryInterface $productRepository;
 
-    public function __construct(Registry $registry, ProductRepositoryInterface $productRepository)
-    {
+    /**
+     * @var Filesystem
+     */
+    private Filesystem $filesystem;
+
+    /**
+     * @param Registry $registry 
+     * @param ProductRepositoryInterface $productRepository 
+     * @param Filesystem $filesystem 
+     */
+    public function __construct(
+        Registry $registry,
+        ProductRepositoryInterface $productRepository,
+        Filesystem $filesystem
+    ) {
         $this->registry = $registry;
         $this->productRepository = $productRepository;
+        $this->filesystem = $filesystem;
     }
 
+    /**
+     * Factory method to create an instance of ProductFixtureRollback
+     * @return ProductFixtureRollback 
+     */
     public static function create(): ProductFixtureRollback
     {
         $objectManager = Bootstrap::getObjectManager();
         return new self(
             $objectManager->get(Registry::class),
-            $objectManager->get(ProductRepositoryInterface::class)
+            $objectManager->get(ProductRepositoryInterface::class),
+            $objectManager->get(Filesystem::class)
         );
     }
 
     /**
-     * @param ProductFixture ...$productFixtures
-     * @throws LocalizedException
+     * Rollback method to delete products and their media gallery images.
+     * 
+     * @param ProductFixture ...$productFixtures 
+     * @return void 
+     * @throws RuntimeException 
+     * @throws FileSystemException 
      */
     public function execute(ProductFixture ...$productFixtures): void
     {
         $this->registry->unregister('isSecureArea');
         $this->registry->register('isSecureArea', true);
 
+        $mediaDir = $this->filesystem->getDirectoryWrite(DirectoryList::MEDIA);
+
         foreach ($productFixtures as $productFixture) {
             try {
+                $product = $productFixture->getProduct();
+
+                // Delete product's media gallery images from filesystem
+                $mediaGalleryEntries = $product->getMediaGalleryEntries();
+                if ($mediaGalleryEntries) {
+                    foreach ($mediaGalleryEntries as $entry) {
+                        $file = ltrim($entry->getFile(), '/');
+                        $path = 'catalog/product/' . $file;
+                        if ($mediaDir->isExist($path)) {
+                            $mediaDir->delete($path);
+                        }
+                    }
+                }
+
                 $this->productRepository->deleteById($productFixture->getSku());
             } catch (\Exception) {
                 // this is fine, products has already been removed
